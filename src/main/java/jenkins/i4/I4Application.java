a8a53e105ee5582daf315f7891acb8f8f26a35ea
RORN Makara <rornmakara89@gmail.com>
2024-03-29 14:12:25 +0700

    build

---
+++ src/main/java/jenkins/i4/I4Application.java
@@ -0,0 +1,13 @@
+package jenkins.i4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I4Application.class, args);
+	}
+
+}
---
+++ src/test/java/jenkins/i4/I4ApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkins.i4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
